54cdb7ceff87c5bd079a1dc6b7d11426552b6c1e
peppo-su <ramanujanmanwho@gmail.com>
2021-06-21 20:52:04 -0500

    m-add 3rd module

---
+++ 03module/decisions.py
@@ -0,0 +1,23 @@
+import os
+os.system("cls")
+
+a = 2 == 2
+b = 2 == 2.
+c = 1 == 2
+print(a,b,c,sep="---")
+
+# var == 0
+# == (equal) operator
+var = 0  # Assigning 0 to var
+print(var == 0)
+
+var = 1  # Assigning 1 to var
+print(var == 0)
+
+# != (not equal) operator
+var = 0  # Assigning 0 to var
+print(var != 0)
+
+var = 1  # Assigning 1 to var
+print(var != 0)
+
---
+++ 03module/labs.py
@@ -0,0 +1,7 @@
+# LAB: Questions and answers
+
+# comparison operators in Python
+# n = int(input("Enter a number: "))
+# print(n >= 100)
+
+print(2 == 2.0)
